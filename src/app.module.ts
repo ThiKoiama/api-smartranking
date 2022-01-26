0b8f8b0026fa582e08a127d8b683e21f7b1420f7
ThiKoiama <thiagokoiama@gmail.com>
2022-01-26 03:52:52 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.g4iwy.mongodb.net/smartranking?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    ),
+    JogadoresModule,
+    CategoriasModule,
+    DesafiosModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
